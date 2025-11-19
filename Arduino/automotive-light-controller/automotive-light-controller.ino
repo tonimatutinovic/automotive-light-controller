0602bee45fc1c8c7aea8ab764eefe4091a1bf52c
Toni Matutinović <tonimatutta@gmail.com>
2025-11-19 09:00:12 +0100

    Add pin definitions and basic variables

--- Arduino/automotive-light-controller/automotive-light-controller.ino
+++ Arduino/automotive-light-controller/automotive-light-controller.ino
@@ -1,3 +1,25 @@
+// Pins
+const int POT_PIN = A0;
+const int DASH_LDR_PIN = A1;
+const int BACK_LDR_PIN = A2;
+
+const int LED_RED_PIN = 11;
+const int LED_GREEN_PIN = 10;
+const int LED_BLUE_PIN = 9;
+
+const int HEADLIGHT_PIN = 2;
+
+// Sensor Values
+int potVal = 0;
+int dashLDRval = 0;
+int backLDRval = 0;
+
+// Threshold
+const int Threshold = 650;
+
+// Delay time between readings
+const unsigned int dt = 250;
+
 void setup() {
   Serial.begin(115200);
 }
