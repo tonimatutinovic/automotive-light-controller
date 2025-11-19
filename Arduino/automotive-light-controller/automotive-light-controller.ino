700b7ceeeb88dfa7c2978821424db2c27bd481a2
Toni Matutinović <tonimatutta@gmail.com>
2025-11-19 10:35:39 +0100

    Add LDR sensor fault detection in AUTO mode
    Detects critical fault when both LDR sensors fail and turns on headlights for safety, handles single sensor faults by operating based on the working sensor while reporting the faulty one.

--- Arduino/automotive-light-controller/automotive-light-controller.ino
+++ Arduino/automotive-light-controller/automotive-light-controller.ino
@@ -33,6 +33,19 @@ unsigned long ldrChangeStart = 0;
 bool ldrLightsOn = false;
 const unsigned long CHANGE_DELAY = 2000;
 
+// Fault detection
+const int LDR_MIN = 20; // LDR reading below this value indicates sensor fault
+const int LDR_MAX = 1003; // LDR reading above than this value indicates sensor fault
+const unsigned long SENSOR_FAULT_DELAY = 5000; // 5 seconds delay for checking sensor fault
+unsigned long dashFaultStart = 0;
+unsigned long backFaultStart = 0;
+bool dashFault = false;
+bool backFault = false;
+
+// Error value
+String error = "NO";
+
+
 // Function for updating LED
 void UpdateLED(int r, int g, int b){
   analogWrite(LED_RED_PIN, r);
@@ -63,8 +76,47 @@ void modeAuto() {
   dashLDRval = analogRead(DASH_LDR_PIN);
   backLDRval = analogRead(BACK_LDR_PIN);
 
-  bool dashActive = (dashLDRval > Threshold);
-  bool backActive = (backLDRval > Threshold);
+  // CHECK DASH LDR
+  if(dashLDRval < LDR_MIN || dashLDRval > LDR_MAX){
+    if(dashFaultStart == 0){
+      dashFaultStart = millis();  // Start timer
+    }
+    else if(millis() - dashFaultStart >= SENSOR_FAULT_DELAY){
+      dashFault = true; // If time passed from last valid dash LDR reading is longer than 5 seconds, sensor fault is detected
+    }
+  }
+  else{
+    dashFault = false;
+    dashFaultStart = 0;
+  }
+  // CHECK BACK LDR
+  if(backLDRval < LDR_MIN || backLDRval > LDR_MAX){
+      if(backFaultStart == 0) 
+        backFaultStart = millis();
+      else if(millis() - backFaultStart >= SENSOR_FAULT_DELAY) 
+        backFault = true; // If time passed from last valid back LDR reading is longer than 5 seconds, sensor fault is detected
+  }else{
+      backFaultStart = 0;
+      backFault = false;
+  }
+
+  // ERROR HANDING
+  // Both sensors not active - system not working
+  if(dashFault && backFault){
+    ldrLightsOn = true; // Turning lights on because of safety reasons
+    error = "CRITICAL_FAULT"; // error status for Serial and  VPython
+  }
+  // One active - system works based on one LDR, shows error message for inactive LDR
+  else if(dashFault)
+    error = "DASH_FAULT";
+  else if(backFault) 
+    error = "BACK_FAULT";
+  // Both active - system works
+  else
+    error = "NO";
+
+  bool dashActive = !dashFault && (dashLDRval > Threshold);
+  bool backActive = !backFault && (backLDRval > Threshold);
 
   // Threshold logic
   if (dashActive || backActive){
@@ -95,7 +147,9 @@ void modeAuto() {
   Serial.print(" backLDR:");
   Serial.print(backLDRval);
   Serial.print(" ");
-  Serial.println(auto_mode);
+  Serial.print(auto_mode);
+  Serial.print(" ");
+  Serial.println(error);
 }
 
 void modeOn() {
